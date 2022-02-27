6d1397f79d2d4f6f7591b52e6639cc412c68c754
SamuelMasters <samuelmastersdev@gmail.com>
2022-02-27 14:04:39 +0000

    Add code to generate a deck of card values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // Setup initial event listeners for player buttons
-
 document.addEventListener('DOMContentLoaded', function() {
     let hitButton = document.getElementById('hit-button');
     let standButton = document.getElementById('stand-button');
@@ -7,10 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
     standButton.addEventListener('click', stand);
 });
 
+// Generate a full deck of 52 'cards' / values
+let cardDeck = [];
+let values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "king", "queen"];
+for (let i = 0; i < values.length; i++) {
+    cardDeck.push(values[i]);
+    cardDeck.push(values[i]);
+    cardDeck.push(values[i]);
+    cardDeck.push(values[i]);
+}
+
 function startGame() {
 
 }
 
+/**
+ * Takes two random values from cardDeck array and
+ * moves them to computerPlayedCards and
+ * humanPlayedCards arrays
+ */
 function dealStartingCards() {
 
 }
@@ -39,8 +53,14 @@ function hit() {
     alert("The 'hit' button was pressed!");
 }
 
+function drawCard() {
+
+}
+
 let computerTotal = '';
 let playerTotal = '';
 let cardInPlay = '';
 let computerScore = '';
 let playerScore = '';
+let computerPlayedCards = [];
+let humanPlayedCards = [];
